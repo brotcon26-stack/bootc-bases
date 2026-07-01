2f207b402166910265b77b8c031cca6abf4e2e60
brotcon26 <brotcon26@gmail.com>
2026-07-01 12:48:43 -0700

    updated build.sh to force pull and no cache

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 TARGET=${1:-}
 
-podman build -t registry.lab/osanwe:$TARGET \
+podman build -t registry.lab/bases:$TARGET \
+    --pull-always --no-cache \
     -f Containerfile.$TARGET \
-    && podman push registry.lab/osanwe:$TARGET
+    && podman push registry.lab/bases:$TARGET
